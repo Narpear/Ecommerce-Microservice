56e110692a60c2e05235f8895a3d0a67a84efc2e
Narpear <prer.kulk@gmail.com>
2024-04-09 13:39:07 +0530

    added styling to homepage

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, session, abort, jsonify
+from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_mysqldb import MySQL
 import yaml
